884e32847d1761808bc265943df603ba2499df43
Atanas Vasilev <atanas.vasilev@primeholding.com>
2025-04-13 21:57:42 +0300

    Implement listBeers() mapped to: GET /api/v1/beer

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,14 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import guru.springframework.spring6restmvc.model.Beer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
+@RestController
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
         log.debug("Getting beer with id: " + id.toString() + " in the controller");
